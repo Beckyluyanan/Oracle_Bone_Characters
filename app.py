823edb0be7335a591b62761ef16e9feed3b7e461
Beckyluyanan <52555026+Beckyluyanan@users.noreply.github.com>
2023-11-17 10:03:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def instruct_and_predict(instruction, input_img=None):
     else:
         result = model.inference(data=input_img) 
         result = model.format_output(lang="zh")
+        score = result['置信度']
         result_text = classes[result['标签']]
         feedback = ""
         if result['置信度'] < 0.99:
@@ -20,16 +21,17 @@ def instruct_and_predict(instruction, input_img=None):
             else:
                 feedback = f"不太对哦，我期望的是：{instruction}，但你画的像：{result_text}。"
 
-        return result_text, feedback
+        return result_text,score, feedback
 
 label_input = gr.Textbox(label="我希望你画的是")
 Image_input = gr.Image(shape=(128, 128), source="canvas", label="画板")
-label_output = gr.Textbox(label="你画的甲骨文是")
+label_output1 = gr.Textbox(label="你画的甲骨文是")
+label_output2 = gr.Textbox(label="识别置信度是")
 Image_output = gr.Textbox(label="反馈")
 
 demo = gr.Interface(fn=instruct_and_predict, 
     inputs=[label_input,Image_input],
-    outputs=[label_output,Image_output],
+    outputs=[label_output1,label_output2,Image_output],
     live=False,
     title="甲骨文学习小游戏",
     description="请在上方文本框输入你希望绘制的甲骨文（例如：“人”或“大”），然后在画板上进行绘制，查看结果。",
